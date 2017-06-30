d858be8966dad0cc33f3725f42555329783730f2
pekko1215 <pekko1215@yahoo.co.jp>
2017-06-30 21:16:09 +0900

    もち！

--- CrankyCollection.js
+++ CrankyCollection.js
@@ -1,11 +1,46 @@
-var CrankyCollection = function(prm1,par2){
-	var ret = "";
-	var query;
-	while(query = prm1.shift()){
-		switch(true){
-			case Array.isArray(query):
-
-			break;
-		}
-	}
+var CrankyCollection = function(prm1, prm2) {
+        var ret = [];
+        var query;
+        var index = 0;
+        var readstart = -1;
+        while (typeof(query = prm1.shift()) !== "undefined") {
+                // console.log(prm2.substr(index))
+                switch (true) {
+                        case Array.isArray(query):
+                                query.findIndex(function(d) {
+                                        var tmp = prm2.indexOf(d, index)
+                                        if (tmp != -1) {
+                                                index = tmp + query.length;
+				return true;
+                                        }
+                                        return false;
+                                })
+                                break;
+                        case typeof query == "object":
+                                break;
+                        case typeof query == "function":
+		        while(query(prm2.substr(index,1))){
+			index++;
+		        }
+		        break;
+                        case typeof query == "string":
+                                index = prm2.indexOf(query, index)+query.length;
+                                break;
+                        case typeof query == "boolean":
+                                if (query) {
+                                        readstart = index
+                                } else {
+                                        ret.push(prm2.substring(readstart, index))
+                                        readstart = -1;
+                                }
+                                break;
+                        case typeof query == "number":
+                                index += query;
+                                break;
+                }
+        }
+        if (readstart != -1) {
+                ret.push(prm2.substring(readstart));
+        }
+        return ret;
 }
